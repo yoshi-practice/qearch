97a75a1a8568f450b770ee611904854e716b89b0
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-05 15:42:08 +0900

    Update Gson model-class

---
+++ app/src/main/java/com/yoshi1125hisa/qearch/Item.kt
@@ -0,0 +1,12 @@
+package com.yoshi1125hisa.qearch
+
+data class Item(
+    val id: String?,
+    val title: String?,
+    val body: String?,
+    val url: String?,
+    val comments_count: Int?,
+    val likes_count: Int?,
+    val created_at: String?,
+    val user: User?
+)
